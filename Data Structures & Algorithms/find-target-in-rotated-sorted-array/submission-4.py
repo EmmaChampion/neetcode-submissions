76dab1a1408b5ff6bf316c3b67f6e5976fa85032
EmmaChampion <277982908+EmmaChampion@users.noreply.github.com>
2026-07-17 22:41:11 -0400

    Add: find-target-in-rotated-sorted-array - submission-4

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-4.py
@@ -0,0 +1,36 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        #Find rotation
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            middle = (left + right) // 2
+            if nums[middle] > nums[right]:
+                left = middle + 1
+            else:
+                right = middle
+        rotation = left
+        print(rotation)
+
+        #Determine which half the number could be in
+        if target == nums[0]:
+            return 0
+        elif target < nums[0] or rotation == 0:
+            #Right half, after pivot
+            left = rotation % len(nums)
+            right = len(nums) - 1
+        else:
+            #Left half, before pivot
+            left = 0
+            right = rotation - 1
+        
+        #Binary search on the half the number could be in
+        while left <= right:
+            middle = (left + right) // 2
+            if target == nums[middle]:
+                return middle
+            elif target < nums[middle]:
+                right = middle - 1
+            else:
+                left = middle + 1
+        return -1

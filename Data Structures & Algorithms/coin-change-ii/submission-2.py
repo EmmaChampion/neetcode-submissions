c1187a0fd4cd3d1cd45ba99835be29a1113d55ef
EmmaChampion <277982908+EmmaChampion@users.noreply.github.com>
2026-08-16 21:27:20 -0400

    Add: coin-change-ii - submission-2

---
+++ Data Structures & Algorithms/coin-change-ii/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        combos = {}
+
+        def dfs(i, goal):
+            if goal < 0:
+                return 0
+            if goal == 0:
+                return 1
+            if (i, goal) in combos:
+                return combos[(i, goal)]
+            combos[(i, goal)] = 0
+            for j in range(i, len(coins)):
+                combos[(i, goal)] += dfs(j, goal - coins[j])
+            return combos[(i, goal)]
+        
+        return dfs(0, amount)

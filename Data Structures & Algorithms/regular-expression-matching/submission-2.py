4e64e007d74b9643c3f60c027a77ba468bc4b864
EmmaChampion <277982908+EmmaChampion@users.noreply.github.com>
2026-08-20 21:41:12 -0400

    Add: regular-expression-matching - submission-2

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        memo = {}
+        m = len(s)
+        n = len(p)
+
+        def dfs(i, j):
+            print(i, j)
+            if j >= n:
+                return i >= m
+            if (i, j) in memo:
+                return memo[(i, j)]
+        
+            match = i < m and (s[i] == p[j] or p[j] == ".")
+            if j + 1 < n and p[j + 1] == "*":
+                if match:
+                    memo[(i, j)] = dfs(i, j+2) or dfs(i+1, j)
+                else:
+                    memo[(i, j)] = dfs(i, j+2)
+                return memo[(i, j)]
+            if match:
+                memo[(i, j)] = dfs(i+1, j+1)
+            else:
+                memo[(i, j)] = False
+            return memo[(i, j)]
+        
+        return dfs(0, 0)
+            
